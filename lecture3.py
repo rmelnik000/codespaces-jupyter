df78e8f808d64c95d31cb8b773e7e9bdc797a245
rmelnik000 <143218965+rmelnik000@users.noreply.github.com>
2023-09-05 16:01:29 +0000

    update the py file with the same come as the notebook

--- lecture3.py
+++ lecture3.py
@@ -1,6 +1,10 @@
-print('Good MORNING')
+"""hi"""
 
-x = True
+import random
+
+#print('Good MORNING')
+
+'''x = True
 
 if x > 2:
     print("x is bigger than 2")
@@ -8,3 +12,26 @@ elif x < 0:
     print('x is negative')
 else:
     print('x is 0, 1, or 2')
+
+
+september = random.randint(0,10)
+print(september)'''
+
+even = 0
+odd = 0
+bars = 0
+
+while bars < 50:
+    value = random.randint(1000,9999)
+
+    if value % 2 == 1:
+        print("value is odd")
+        odd += 1
+    else: 
+        print ("value is even")
+        even += 1
+    print("value is", value)
+
+    print("evens:", even)
+    print("odds:", odd)
+    bars += 1
